298fd51eabeac0aca3b2b5abef2152afc153ead2
unknown <euluishenrique7@gmail.com>
2025-10-14 14:59:24 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,119 @@
-import Image from "next/image";
+"use client";
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [imagemPreview, setImagemPreview] = useState<string>();
+  const [arquivo, setArquivo] = useState<File>();
+  const [status, setStatus] = useState<string>();
+  const [resultados, setResultados] = useState<string[]>([]);
+  const [csvUrl, setCsvUrl] = useState<string>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImagemPreview(URL.createObjectURL(file));
+      setArquivo(file);
+    }
+  }
+
+  async function handleUpload(event: React.FormEvent) {
+    event.preventDefault(); 
+
+    if (!arquivo) {
+      setStatus("Selecione um arquivo antes de enviar!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", arquivo);
+
+    try {
+      const response = await fetch("http://localhost:5000/process", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) throw new Error("Falha no upload");
+
+      const data = await response.json();
+      console.log(data);
+      setStatus("Upload realizado com sucesso!");
+
+      const imagens: string[] = [];
+      let csv: string = "";
+      data.results.forEach((url: string) => {
+        if (url.endsWith(".csv")) {
+          csv = url;
+        } else {
+          imagens.push(url);
+        }
+      });
+
+      setResultados(imagens);
+      setCsvUrl(csv);
+
+    } catch (err) {
+      console.error(err);
+      setStatus("Erro ao enviar a imagem");
+    }
+
+  }
+
+  return (
+    <div className="main">
+      <Image
+        src="/assets/logo.png"
+        height={300}
+        width={300}
+        alt="Logo"
+      />
+
+      <form onSubmit={handleUpload} className="uploadFotoForm">
+        <input
+          id="uploadFoto"
+          name="uploadFoto"
+          type="file"
+          accept="image/*"
+          className="inputForm"
+          onChange={handleFileChange}
+        />
+
+        <label htmlFor="uploadFoto" className="labelForm"> Selecionar imagem </label>
+
+        {imagemPreview && (
+          <div className="PreviewBox">
+            <img
+              src={imagemPreview}
+              alt="Pré-visualização"
+              className="imgPreview"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          </div>
+        )}
+
+        <button type="submit" className="btEnviar">Enviar</button>
+
+        {status && <p>{status}</p>}
+      </form>
+
+      {resultados.length > 0 && (
+        <div className="resultados">
+          <h2>Imagens Processadas:</h2>
+          {resultados.map((url, index) => (
+            <div key={index} className="imgBox">
+              <img src={url} alt={`Processado ${index}`} style={{ maxWidth: "300px", margin: "10px" }} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      {csvUrl && (
+        <div className="csvDownload">
+          <a href={csvUrl} download>
+            Baixar CSV com resultados
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
